e8414d40af510a96510f427292956227b5af497f
Mustaq <mustaq.ahmed.codes@gmail.com>
2023-02-08 23:13:56 +0530

    changes

--- app.js
+++ app.js
@@ -38,16 +38,16 @@ app.use("/api", paymentRoutes)
 app.use("/api", contactRoutes)
 
 // Serve Frontend
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static('client/build'))
-//     app.get('*', (req, res) =>
-//         res.sendFile(
-//             path.resolve(__dirname, 'client', 'build', 'index.html')
-//         )
-//     )
-// }else{
-//     app.get("/", (req, res) => res.send("Please Setup to Production")) 
-// }
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, 'client', 'build', 'index.html')
+        )
+    )
+}else{
+    app.get("/", (req, res) => res.send("Please Setup to Production")) 
+}
 
 // Port
 const port = process.env.PORT || 8000
